96506396250d4e08b5999ddb95fc764b00a93e5b
shruti <shruti.kadam@stcpl.com>
2025-08-13 10:33:32 +0530

    todays changes 130825 1st

--- src/app/features/equity/equity.routes.ts
+++ src/app/features/equity/equity.routes.ts
@@ -1,20 +1,16 @@
+
 import { Routes } from '@angular/router';
-import { EquityComponent } from './equity.component';
 
 export const equityRoutes: Routes = [
     {
         path: '',
-        component: EquityComponent,
-        children: [
-            {
-                path: '',
-                redirectTo: 'mutual-funds',
-                pathMatch: 'full'
-            },
-            {
-                path: 'mutual-funds',
-                loadComponent: () => import('./mutual-funds/mutual-funds.component').then(c => c.MutualFundsComponent)
-            },
-        ]
-    }
+        redirectTo: 'mutual-funds',
+        pathMatch: 'full'
+    },
+    {
+        path: 'mutual-funds',
+        loadComponent: () => import('./mutual-funds/mutual-funds.component').then(c => c.MutualFundsComponent)
+    },
+
+
 ];
