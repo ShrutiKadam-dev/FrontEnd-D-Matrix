1f01cc9293f5dd1239ef7848997c2b37ba056da2
Roshan <roshan.tambe@stcpl.com>
2025-08-14 18:30:18 +0530

    card section and SOA is added in AIF

--- src/app/features/equity/aif/aif.component.ts
+++ src/app/features/equity/aif/aif.component.ts
@@ -1,15 +1,30 @@
+
 import { Component } from '@angular/core';
-import { CarouselModule } from 'primeng/carousel';
-import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
 import { AutoCompleteModule } from 'primeng/autocomplete';
+import { CarouselModule } from 'primeng/carousel';
+import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-aif',
-  imports: [CarouselModule, CardModule, AutoCompleteModule],
+  standalone: true,
+  imports: [ InputTextModule, FormsModule, AutoCompleteModule, CarouselModule, TableModule ],
   templateUrl: './aif.component.html',
-  styleUrl: './aif.component.scss'
+  styleUrls: ['./aif.component.scss']
 })
+
+
+
 export class AifComponent {
+
+   actionTableList = [
+    { id: 1, scrip_name: 'ABC Ltd', unit: 100, order_date: '2025-08-10', order_type: 'Buy', purchase_amount: 5000 },
+    { id: 2, scrip_name: 'XYZ Corp', unit: 50, order_date: '2025-08-11', order_type: 'Sell', purchase_amount: 2500 },
+    { id: 3, scrip_name: 'LMN Pvt', unit: 200, order_date: '2025-08-12', order_type: 'Buy', purchase_amount: 10000 },
+    { id: 4, scrip_name: 'QRS Inc', unit: 75, order_date: '2025-08-13', order_type: 'Sell', purchase_amount: 3750 },
+    { id: 5, scrip_name: 'TUV Group', unit: 120, order_date: '2025-08-14', order_type: 'Buy', purchase_amount: 6000 }
+  ];
 
     aifList = [
     { name: 'AIF One', value: '₹1,00,000', entityId: 'ENT-001' },
